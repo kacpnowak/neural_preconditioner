cf8c9cd8cc2ec0da562ca977a1344f9819389c7c
Kacper Nowak <kacper.nowak@awi.de>
2025-06-27 08:10:57 +0200

    Add spectral radius

--- GNP.py
+++ GNP.py
@@ -6,6 +6,32 @@ import os
 import numpy as np
 from tqdm import tqdm
 
+def scale_A_by_spectral_radius(A):
+
+    if A.layout == torch.sparse_csc:
+
+        absA = torch.absolute(A)
+        m, n = absA.shape
+        row_sum = absA @ torch.ones(n, 1, dtype=A.dtype, device=A.device)
+        col_sum = torch.ones(1, m, dtype=A.dtype, device=A.device) @ absA
+        gamma = torch.min(torch.max(row_sum), torch.max(col_sum))
+        outA = A * (1. / gamma.item())
+
+    elif A.layout == torch.strided:
+
+        absA = torch.absolute(A)
+        row_sum = torch.sum(absA, dim=1)
+        col_sum = torch.sum(absA, dim=0)
+        gamma = torch.min(torch.max(row_sum), torch.max(col_sum))
+        outA = A / gamma
+
+    else:
+
+        raise NotImplementedError(
+            'A must be either torch.sparse_csc_tensor or torch.tensor')
+
+    return outA
+
 
 class Arnoldi():
     def build(self, A, v0=None, m=100):
